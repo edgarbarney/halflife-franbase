9dda6bc7d4cd0ae49d64ca6568392b7bb62882d5
FranticDreamer <edwardgmgmd@gmail.com>
2024-11-29 16:42:42 +0300

    Fix sscanf Warnings

--- cl_dll/renderer/propmanager.cpp
+++ cl_dll/renderer/propmanager.cpp
@@ -569,13 +569,13 @@ void CPropManager::LoadEntVars(void)
 			pValue = ValueForKey(&m_pBSPEntities[i], "skin");
 
 			if (pValue)
-				sscanf(pValue, "%d", &m_pEntities[m_iNumEntities].curstate.skin);
+				sscanf(pValue, "%hd", &m_pEntities[m_iNumEntities].curstate.skin);
 
 
 			pValue = ValueForKey(&m_pBSPEntities[i], "scale");
 
 			if (pValue)
-				sscanf(pValue, "%d", &m_pEntities[m_iNumEntities].curstate.scale);
+				sscanf(pValue, "%f", &m_pEntities[m_iNumEntities].curstate.scale);
 
 
 			pValue = ValueForKey(&m_pBSPEntities[i], "renderfx");
